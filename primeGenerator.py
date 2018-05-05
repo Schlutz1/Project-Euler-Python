12a9ed91034a2c9543ad0b1d87772dfcf4215481
Max Schultz <max.schultz1@gmail.com>
2018-05-05 14:48:24 +1000

    add dict method for looksups

--- primeGenerator.py
+++ primeGenerator.py
@@ -2,6 +2,7 @@
 
 import math
 
+#returns list, this is faster for iteration, but only slighter
 def prime_generator(n) :
 	sieve = [True] * (n+1)
 	ps = []
@@ -10,4 +11,16 @@ def prime_generator(n) :
 			for i in range(p**2, n, p):
 				sieve[i] = False
 			ps.append(p)
+	return ps
+
+
+#this might be faster for lookup i.e. checking
+def prime_generator_dictionary(n) :
+	sieve = [True] * (n+1)
+	ps = {}
+	for p in range(2, n) :
+		if sieve[p] :
+			for i in range(p**2, n, p) :
+				sieve[i] = False
+			ps[p]=p
 	return ps
