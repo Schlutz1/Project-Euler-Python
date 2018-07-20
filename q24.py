9fe527aa3141df167d86d4f3cfbad1b74fb1f609
Schlutz1 <max.schultz1@gmail.com>
2018-07-20 19:07:49 +1000

    updates

--- q24.py
+++
@@ -1,9 +0,0 @@
-#q24 python
-import itertools, sys
-
-
-q = '0123456789'
-
-temp = itertools.islice(itertools.permutations(q), 999999, None)
-print "".join(str(x) for x in next(temp))
-
--- q48.py
+++
@@ -1,8 +0,0 @@
-#q48 - self powers
-
-total = 0
-
-for val in range(1, 1001) :
-	total = total + val** val
-
-print total
