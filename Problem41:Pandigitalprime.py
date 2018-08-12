d4817405efc7f37f68f7c91b110b0de3caeac548
Schlutz1 <max.schultz1@gmail.com>
2018-08-12 13:30:18 +1000

    pandigitals

--- Problem41:Pandigitalprime.py
+++ Problem41:Pandigitalprime.py
@@ -1,11 +1,31 @@
-from primeGenerator import prime_generator_dictionary
+'''
+Problem 41
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. 
+For example, 2143 is a 4-digit pandigital and is also prime.
 
-UPPER_LIMIT = 3000
+What is the largest n-digit pandigital prime that exists?
+'''
 
-prime_dict = prime_generator_dictionary(UPPER_LIMIT)
+#look at permutation of n-digit pandigitials
+
+#start with n = 9, and look at all permutations not ending in an even number
+#dec n until a prime is found 
 
 
-print "done"
-#for n in range()
+def is_pandigital(q) :
+	q = str(q)
+	n = len(q)
+	beg=set(q[0:n])
+	end=set(q[-n:]) #checks if vals are 1 - n
+	return beg==end and beg==set(map(str, range(1, n + 1))) #checks if each val is unique
 
-#temp = itertools.islice(itertools.permutations(q), 999999, None)
+
+from resource_primeGenerator import prime_generator
+
+p_list = prime_generator(7654321)
+
+for counter in range(len(p_list)-1, -1, -1) :
+	val = p_list[counter]
+	if is_pandigital(val) :
+		print val
+		break
